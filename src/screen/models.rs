2569abfd287a1c669929379cbccb724024c7ee7f
jose miguel mut <jomutlo@gmail.com>
2023-04-14 01:17:40 +0100

    comment out loading meshes and downgrade macroquad
    for some reason the web version started complaining about
    a version mismatch. the gl.js bundle is 0.3.13, but miniquad was 0.3.14.
    
    I donwgraded macroquad to 0.3.24 because it uses miniquad 0.3.13

--- src/screen/models.rs
+++ src/screen/models.rs
@@ -24,41 +24,42 @@ pub fn load_models() -> Result<Models, AnyError> {
 }
 
 fn gltf_to_meshes(path: &str) -> Result<Vec<Mesh>, AnyError> {
-    let (document, buffers, _images) = gltf::import(path)?;
-    let mut meshes = Vec::<Mesh>::new();
-    for mesh in document.meshes() {
-        for primitive in mesh.primitives() {
-            let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
-
-            if let Some(_accessor) = primitive.get(&Semantic::Positions) {
-                let positions: Vec<[f32; 3]> = reader
-                    .read_positions()
-                    .expect("Position primitives should be present")
-                    .collect();
-                let vertices = positions
-                    .iter()
-                    // .zip(normals.iter())
-                    // .zip(tex_coords.into_f32().iter())
-                    // .map(|((&[x, y, z], &[nx, ny, nz]), &[u, v])| Vertex::new(x, y, z, u, v, BLACK))
-                    .map(|&[x, y, z]| Vertex {
-                        position: Vec3::new(x, y, z),
-                        uv: Vec2::new(0.0, 0.0),
-                        color: BLACK,
-                    })
-                    .collect::<Vec<_>>();
-
-                let nested_iterator: Option<Map<_, _>> = reader
-                    .read_indices()
-                    .map(|indices| indices.into_u32().map(|i| i as u16));
-                let indices = nested_iterator.expect("").collect::<Vec<_>>();
-                let mesh = Mesh {
-                    vertices,
-                    indices,
-                    texture: None,
-                };
-                meshes.push(mesh);
-            }
-        }
-    }
-    Ok(meshes)
+    return Ok(Vec::new());
+    // let (document, buffers, _images) = gltf::import(path)?;
+    // let mut meshes = Vec::<Mesh>::new();
+    // for mesh in document.meshes() {
+    //     for primitive in mesh.primitives() {
+    //         let reader = primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+    //
+    //         if let Some(_accessor) = primitive.get(&Semantic::Positions) {
+    //             let positions: Vec<[f32; 3]> = reader
+    //                 .read_positions()
+    //                 .expect("Position primitives should be present")
+    //                 .collect();
+    //             let vertices = positions
+    //                 .iter()
+    //                 // .zip(normals.iter())
+    //                 // .zip(tex_coords.into_f32().iter())
+    //                 // .map(|((&[x, y, z], &[nx, ny, nz]), &[u, v])| Vertex::new(x, y, z, u, v, BLACK))
+    //                 .map(|&[x, y, z]| Vertex {
+    //                     position: Vec3::new(x, y, z),
+    //                     uv: Vec2::new(0.0, 0.0),
+    //                     color: BLACK,
+    //                 })
+    //                 .collect::<Vec<_>>();
+    //
+    //             let nested_iterator: Option<Map<_, _>> = reader
+    //                 .read_indices()
+    //                 .map(|indices| indices.into_u32().map(|i| i as u16));
+    //             let indices = nested_iterator.expect("").collect::<Vec<_>>();
+    //             let mesh = Mesh {
+    //                 vertices,
+    //                 indices,
+    //                 texture: None,
+    //             };
+    //             meshes.push(mesh);
+    //         }
+    //     }
+    // }
+    // Ok(meshes)
 }
