7e384497d5a266d017afa22fdf0a257929637e21
jose miguel mut <jomutlo@gmail.com>
2023-04-15 12:54:59 +0100

    extract common code

--- src/screen/gui.rs
+++ src/screen/gui.rs
@@ -18,18 +18,9 @@ pub mod jump_button {
     static mut LAST_IS_CLICKED: bool = false;
 
     pub fn is_clicked() -> bool {
-        if is_mouse_button_down(MouseButton::Left) && inside_button(mouse_position_local()) {
-            unsafe { LAST_IS_CLICKED = true };
-            return true;
-        }
-        for touch in touches_local() {
-            if inside_button(touch.position) {
-                unsafe { LAST_IS_CLICKED = false };
-                return true;
-            }
-        }
-        unsafe { LAST_IS_CLICKED = false };
-        return false;
+        let clicked = clicked_or_touched(inside_button);
+        unsafe { LAST_IS_CLICKED = clicked };
+        return clicked;
     }
 
     fn inside_button(position: Vec2) -> bool {
@@ -37,28 +28,7 @@ pub mod jump_button {
     }
 
     pub fn draw() {
-        let screen_h = screen_height();
-        let screen_w = screen_width();
-        let padding = (0.05 * screen_w).min(0.05 * screen_h);
-        let x = 0.5 * screen_w + padding;
-        let y = 0.5 * screen_h + padding;
-        let w = 0.4 * screen_w;
-        let h = 0.15 * screen_h;
-        let color = if unsafe { LAST_IS_CLICKED } {
-            PRESSED_BUTTON_COLOR
-        } else {
-            BUTTON_COLOR
-        };
-        draw_rectangle(x, y, w, h, color);
-        let text = "JUMP";
-        let size = measure_text(text, None, FONT_SIZE as u16, 1.0);
-        draw_text(
-            text,
-            x + (w - size.width) * 0.5,
-            y + h * 0.5,
-            FONT_SIZE,
-            BLACK,
-        );
+        draw_button("JUMP", Vec2::new(0.5, 0.5), unsafe { LAST_IS_CLICKED });
     }
 }
 
@@ -68,18 +38,9 @@ pub mod piss_button {
     static mut LAST_IS_CLICKED: bool = false;
 
     pub fn is_clicked() -> bool {
-        if is_mouse_button_down(MouseButton::Left) && inside_button(mouse_position_local()) {
-            unsafe { LAST_IS_CLICKED = true };
-            return true;
-        }
-        for touch in touches_local() {
-            if inside_button(touch.position) {
-                unsafe { LAST_IS_CLICKED = false };
-                return true;
-            }
-        }
-        unsafe { LAST_IS_CLICKED = false };
-        return false;
+        let clicked = clicked_or_touched(inside_button);
+        unsafe { LAST_IS_CLICKED = clicked };
+        return clicked;
     }
 
     fn inside_button(position: Vec2) -> bool {
@@ -87,27 +48,44 @@ pub mod piss_button {
     }
 
     pub fn draw() {
-        let screen_h = screen_height();
-        let screen_w = screen_width();
-        let padding = (0.05 * screen_w).min(0.05 * screen_h);
-        let x = 0.5 * screen_w + padding;
-        let y = 0.75 * screen_h + padding;
-        let w = 0.4 * screen_w;
-        let h = 0.15 * screen_h;
-        let color = if unsafe { LAST_IS_CLICKED } {
-            PRESSED_BUTTON_COLOR
-        } else {
-            BUTTON_COLOR
-        };
-        draw_rectangle(x, y, w, h, color);
-        let text = "PISS";
-        let size = measure_text(text, None, FONT_SIZE as u16, 1.0);
-        draw_text(
-            text,
-            x + (w - size.width) * 0.5,
-            y + h * 0.5,
-            FONT_SIZE,
-            BLACK,
-        );
+        draw_button("PISS", Vec2::new(0.5, 0.75), unsafe { LAST_IS_CLICKED });
     }
+}
+
+/// inside_button() will receive screen coordinates in the range of {x: [-1, 1], y: [-1, 1]}
+fn clicked_or_touched<F: Fn(Vec2) -> bool>(inside_button: F) -> bool {
+    if is_mouse_button_down(MouseButton::Left) && inside_button(mouse_position_local()) {
+        return true;
+    }
+    for touch in touches_local() {
+        if inside_button(touch.position) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// position is in the range of {x: [0, 1], y: [0, 1]}
+pub fn draw_button(text: &str, position: Vec2, button_is_clicked: bool) {
+    let screen_h = screen_height();
+    let screen_w = screen_width();
+    let padding = (0.05 * screen_w).min(0.05 * screen_h);
+    let x = position.x * screen_w + padding;
+    let y = position.y * screen_h + padding;
+    let w = 0.4 * screen_w;
+    let h = 0.15 * screen_h;
+    let color = if button_is_clicked {
+        PRESSED_BUTTON_COLOR
+    } else {
+        BUTTON_COLOR
+    };
+    draw_rectangle(x, y, w, h, color);
+    let size = measure_text(text, None, FONT_SIZE as u16, 1.0);
+    draw_text(
+        text,
+        x + (w - size.width) * 0.5,
+        y + h * 0.5,
+        FONT_SIZE,
+        BLACK,
+    );
 }
